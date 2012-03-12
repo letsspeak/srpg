a1bc84279f4f4dcc68306f803ebd540e017d43f4
letsspeak <letsspeak21@gmail.com>
2012-03-13 08:26:31 +0900

    Initial Commit

---
+++ SRPG/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  SRPG
+//
+//  Created by masa on 12/03/13.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ SRPG/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SRPG
+//
+//  Created by masa on 12/03/13.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
